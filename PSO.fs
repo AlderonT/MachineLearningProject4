63d546ef77474fa80c2d63200e832b94e1d40096
chrism <chrismichelmajor@gmail.com>
2019-11-25 09:45:22 -0700

    Added module for PSO

---
+++ PSO.fs
@@ -0,0 +1,42 @@
+//--------------------------------------------------------------------------------------------------------------
+//
+//  CSCI 447 - Machine Learning
+//  Assignment #4, Fall 2019
+//  Chris Major, Farshina Nazrul-Shimim, Tysen Radovich, Allen Simpson
+//
+//  Module for the implementation of a Particle Swarm Optimization algorithm
+//
+//--------------------------------------------------------------------------------------------------------------
+
+// MODULES
+//--------------------------------------------------------------------------------------------------------------
+    
+// Open the modules
+open Tools
+open Tools.Extensions
+
+// Declare as a module
+module rec ParticleSwarmOptimization = 
+
+
+// OBJECTS
+//--------------------------------------------------------------------------------------------------------------
+        
+    // (none)
+       
+
+// FUNCTIONS
+//--------------------------------------------------------------------------------------------------------------
+       
+    // Function to run Particle Swarm Optimization given a population
+    let particleSwarmOpt =
+        1
+
+
+// IMPLEMENTATIONS
+//--------------------------------------------------------------------------------------------------------------
+           
+    let var1 = particleSwarmOpt
+    
+//--------------------------------------------------------------------------------------------------------------
+// END OF CODE

e140b1d792255203ecdbea2d41ab032bae88ddb7
chrism <chrismichelmajor@gmail.com>
2019-11-25 13:36:47 -0700

    Code in progress for PSO

--- PSO.fs
+++ PSO.fs
@@ -29,9 +29,35 @@ module rec ParticleSwarmOptimization =
 //--------------------------------------------------------------------------------------------------------------
        
     // Function to run Particle Swarm Optimization given a population
-    let particleSwarmOpt =
-        1
+    let particleSwarmOpt (net : Network) =
+        
+        // Initialize the population as the layer ConnectionMatrix of the network
+        let population = net.layers
 
+        population
+        |> Array.toSeq                                  // Convert the array to a sequence
+        |> Seq.map (fun p ->                            // Set the best values
+            {
+                
+                // Set the personal best
+            
+                // Set the regional (local) best
+        
+            })
+        |> Seq.map (fun p ->                            // Update velocity and position
+            {
+
+                // Generate random r1 and r2
+
+            
+                // Set the velocity
+                // v[i][j][t + 1] = v[i][j][t] + (c1 * r1 * (pb[i][j][t] - x[i][j][t])) + (c2 * r2 * (lb[i][j][t] - x[i][j][t]))
+
+
+                // Set the position
+                // x[i][j][t+1] = x[i][j][t] + v[i][j][t+1];
+        
+            })
 
 // IMPLEMENTATIONS
 //--------------------------------------------------------------------------------------------------------------
